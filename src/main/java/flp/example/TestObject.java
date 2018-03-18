3bb55dd3a0e90c7c8543dd2b3d96b3fe76f7a7ca
FlippinPizza <flip42@gmail.com>
2018-03-18 22:18:56 +0100

    travis

---
+++ src/main/java/flp/example/TestObject.java
@@ -0,0 +1,68 @@
+package flp.example;
+
+public class TestObject {
+	private int integerValue = 42;
+	private double doubleValue = 4.2e-10;
+	private short shortValue = 888;
+	private byte byteValue = 88;
+	private char charValue = 'e';
+	private String stringValue = "string";
+	private boolean boolValue = true;
+
+	public int getIntegerValue() {
+		return integerValue;
+	}
+
+	public void setIntegerValue(int integerValue) {
+		this.integerValue = integerValue;
+	}
+
+	public double getDoubleValue() {
+		return doubleValue;
+	}
+
+	public void setDoubleValue(double doubleValue) {
+		this.doubleValue = doubleValue;
+	}
+
+	public short getShortValue() {
+		return shortValue;
+	}
+
+	public void setShortValue(short shortValue) {
+		this.shortValue = shortValue;
+	}
+
+	public byte getByteValue() {
+		return byteValue;
+	}
+
+	public void setByteValue(byte byteValue) {
+		this.byteValue = byteValue;
+	}
+
+	public char getCharValue() {
+		return charValue;
+	}
+
+	public void setCharValue(char charValue) {
+		this.charValue = charValue;
+	}
+
+	public String getStringValue() {
+		return stringValue;
+	}
+
+	public void setStringValue(String stringValue) {
+		this.stringValue = stringValue;
+	}
+
+	public boolean isBoolValue() {
+		return boolValue;
+	}
+
+	public void setBoolValue(boolean boolValue) {
+		this.boolValue = boolValue;
+	}
+
+}
